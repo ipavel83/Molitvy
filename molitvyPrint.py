cb4d01d4062593a29c09812c95a045f207680450
ipavel83 <ipavel83@gmail.com>
2024-01-23 19:45:19 +0300

    readint number depends on number of tabs

--- molitvyPrint.py
+++ molitvyPrint.py
@@ -9,10 +9,11 @@ def strip_accents(s):
    return ''.join(c for c in unicodedata.normalize('NFD', s)
                   if unicodedata.category(c) != 'Mn')
 
-def printMolitvu(s):
-    for t in s:   
-        print(t)
-        time.sleep(1)
+def printMolitvu(textArray, times):
+    for num in range(0,times):
+        for t in textArray:   
+            print(t)
+            time.sleep(0.05*len(t))
 
 if __name__ == '__main__':
 
@@ -22,13 +23,24 @@ if __name__ == '__main__':
         with open(filename, encoding="utf8") as txt:
             textToExtract =txt.read()
             textAsArray = strip_accents(textToExtract).splitlines()
+            
             t = []
+            times = 0;
+            
             for s in textAsArray:
                 if(s == ''):
                     if t != []:
-                        printMolitvu(t);
+                        printMolitvu(t, times);
+                        
                     t = []
+                    times = 0;
+                    
                     time.sleep(1)
+                elif (s[0] == '\t'):
+                    for tabs in s:
+                        if tabs == '\t':
+                            times = times + 1
+                            
                 t.append(s)
         
     except IOError:
